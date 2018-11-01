5d8655e1e77211fd630296d9f3d6e66a78bc38c3
Ethan Furman <ethan@stoneleaf.us>
2018-11-01 14:09:06 -0700

    make status bar automatically reflect actual restriction (fal-t350)

--- hr_workers_comp.py
+++ hr_workers_comp.py
@@ -1,6 +1,7 @@
 #imports
 from __future__ import print_function
 import logging
+from aenum import NamedTuple
 from datetime import timedelta
 from fnx import date
 from openerp.exceptions import ERPError
@@ -44,11 +45,7 @@ class hr_workers_comp_claim(osv.Model):
             'Status',
             sort_order='definition',
             ),
-        'restriction_state': fields.selection(
-            [('full', 'Full Restriction'), ('light', 'Light Duty'), ('none', 'None')],
-            'Duty',
-            sort_order='definition',
-            ),
+        'restriction_state_id': fields.many2one('hr.workers_comp.duty_type', 'Duty'),
         'employee_id': fields.many2one('hr.employee', 'Employee', ondelete='restrict'),
         'notification_date': fields.date('Notified on', help='Date employee notified us of injury'),
         'injury_ids': fields.many2many(
@@ -129,13 +126,6 @@ class hr_workers_comp_claim(osv.Model):
 
     _defaults = {
         'state': 'open',
-        'restriction_state': 'full',
-        'injury_date': fields.date.context_today,
-        # 'notes_ids': lambda s, c, uid, ctx:
-        #         [{
-        #             'evaluation_date': fields.date.context_today(s, c, uid, context=ctx),
-        #             'note': '<add text and action>', 'write_uid': uid,
-        #             }],
         }
 
     def write(self, cr, uid, ids, values, context=None):
@@ -148,7 +138,7 @@ class hr_workers_comp_claim(osv.Model):
         res = {}
         res['value'] = value = {}
         today = date(fields.date.context_today(self, cr, uid, context=context))
-        injury_date = date(injury)
+        today_added = False
         restricted = DayCounter()
         no_duty = DayCounter()
         if not notes_ids:
@@ -159,46 +149,59 @@ class hr_workers_comp_claim(osv.Model):
                         'restriction_state': 'none',
                         })
             return res
+        ir_model_data = self.pool.get('ir.model.data')
+        no_duty_restriction = 'none'
+        incomplete = ir_model_data.get_object(cr, uid, 'hr_workers_comp', 'incomplete')
         note_history = self.pool.get('hr.workers_comp.history')
         duty_type = self.pool.get('hr.workers_comp.duty_type')
-        duty_restrictions = dict([(r['id'], r['restriction']) for r in duty_type.read(cr, uid, context=context)])
+        duty_restrictions = dict([
+            (r['id'], Restriction(r['id'], r['name'], r['restriction']))
+            for r in duty_type.read(cr, uid, context=context)
+            ])
         notes = []
         if isinstance(notes_ids[0], list):
             # from web form
             for note in notes_ids:
                 if note[0] == 0:
                     # create
-                    # [0, False, {'note': False, 'evaluation_date': '2017-02-02', 'duty_id': 22}]
+                    # [0, False, {'note': False, 'evaluation_date': '2017-02-02', 'duty_id': 22, 'restriction': 'est'}]
                     duty_id = note[2].get('duty_id')
                     if not duty_id:
                         continue
                     try:
                         duty = duty_restrictions[duty_id]
                         eval_date = date(note[2]['evaluation_date'])
-                        if duty != 'na':
-                            notes.append((duty, eval_date))
+                        restriction = note[2].get('restriction') or duty.restriction or False
+                        if not restriction in ('est', 'na'):
+                            notes.append((restriction, eval_date, duty))
                     except Exception:
                         _logger.exception('bad note: %r', note)
                         raise
                 elif note[0] == 1:
                     # update (so read old record and apply updates)
-                    # [1, 15, {'evaluation_date': '2017-01-17'}]
+                    # [1, 15, {'evaluation_date': '2017-01-17', 'duty_id': 13, 'restriction': 'full'}]
+                    #
+                    # new style note or old style?
                     note_update = note[2]
                     note = note_history.browse(cr, uid, note[1], context=context)
+                    restriction = note_update.get('restriction') or note.restriction
+                    if not restriction:
+                        # old style: restriction is in duty_id -- abort calculations
+                        return {}
+                    elif restriction in ('na', 'est', False):
+                        continue
                     duty_id = note_update.get('duty_id')
                     if duty_id is None:
-                        restriction = note.duty_id.restriction
-                    elif duty_id in ('na', False):
-                        continue
+                        duty_id = note.duty_id
                     else:
-                        restriction = duty_restrictions[duty_id]
+                        duty_id = duty_restrictions[duty_id]
                     eval_date = date(note_update.get('evaluation_date'))
                     if not eval_date:
                         eval_date = date(note.evaluation_date)
-                    if restriction is None or eval_date is False:
+                    if eval_date is False:
                         # no recorded restriction and/or date, nothing we can calculate with this record
                         continue
-                    notes.append((restriction, eval_date))
+                    notes.append((restriction, eval_date, duty))
                 elif note[0] in (2, 3, 5):
                     # various flavors of unlink
                     pass
@@ -206,40 +209,39 @@ class hr_workers_comp_claim(osv.Model):
                     # link
                     # [[4, 10, False]]
                     note = note_history.browse(cr, uid, note[1], context=context)
-                    duty = note.duty_id.restriction
-                    if duty in ('na', False):
+                    restriction = note.restriction
+                    if restriction in ('na', 'est', False):
                         continue
                     eval_date = date(note.evaluation_date)
-                    notes.append((note.duty_id.restriction, date(note.evaluation_date)))
+                    notes.append((note.restriction, date(note.evaluation_date)), note.duty_id)
         else:
             # from function field
             # [note1, note2, note3, ...]
             notes = [
-                    (note.duty_id.restriction, date(note.evaluation_date))
+                    (note.restriction, date(note.evaluation_date), note.duty_id)
                     for note in notes_ids
-                    if note.duty_id.restriction != 'na'
+                    if note.restriction not in ('na', 'est', False)
                     ]
-        if notes and notes[-1][1] < today:
-            notes.append((None, today))
-        last_duty = 'none'
-        last_restriction = 'none'
-        last_date = injury_date
-        for restriction_level, eval_date in notes:
-            if restriction_level is None:
-                # fix entry for today
-                restriction_level = 'none'
-            elif restriction_level != 'est_none':
-                last_duty = restriction_level
+        last_duty = incomplete
+        if notes:
+            last_restriction = notes[0][0]
+            last_date = notes[0][1]
+            last_duty = notes[0][2]
+            if notes[-1][1] < today:
+                notes.append((no_duty_restriction, today, None))
+                today_added = True
+        for restriction, eval_date, duty in notes[1:]:
+            last_duty = duty
             # calculate number of days in last state
             if last_restriction == 'none':
                 # unless those were no-restriction days
-                last_restriction = restriction_level
+                last_restriction = restriction
                 last_date = eval_date
                 continue
-            if last_restriction in ('na', restriction_level):
+            if last_restriction == restriction:
                 continue
             days = eval_date - last_date - ONE_DAY
-            if last_restriction in ('light', 'full') and restriction_level in ('light', 'full'):
+            if last_restriction in ('light', 'full') and restriction in ('light', 'full'):
                 days += ONE_DAY
             if last_restriction == 'full':
                 no_duty += days
@@ -247,9 +249,11 @@ class hr_workers_comp_claim(osv.Model):
                 restricted += days
             else:
                 raise ERPError('Bug!', 'unknown restriction state: %r' % last_restriction)
-            last_restriction = restriction_level
+            last_restriction = restriction
             last_date = eval_date
-        value['restriction_state'] = last_duty
+        if today_added:
+            last_duty = notes[-2][2]
+        value['restriction_state_id'] = last_duty.id
         value['total_days'] = int(restricted + no_duty)
         value['restricted_duty_total'] = int(restricted)
         value['no_duty_total'] = int(no_duty)
@@ -294,29 +298,27 @@ class hr_workers_comp_duty_type(osv.Model):
     _desc = "workers comp claim duty type"
 
     _columns = {
+        'active': fields.boolean('Active'),
         'name': fields.char('Description', size=128),
         'restriction': fields.selection((
-            ('full', 'No duties'),
-            ('light', 'Light Duties'),
-            ('none', 'Normal duties'),
-            ('na', 'N/A'),
+                ('full', 'No duties'),
+                ('light', 'Light Duties'),
+                ('none', 'Normal duties'),
+                ('na', 'N/A'),
             ),
-            string='Restriction level',
-            required=True,
-            help='No Duties -> Full restriction, no work\n'
-                 'Light Duties -> light duties\n'
-                 'Normal Duties -> No restrictions, normal work\n'
-                 'N/A -> No longer employed',
+            'Restriction level',
             ),
         }
 
-    _sql_constraints = [
-            ('duty_uniq', 'unique(name)', 'This duty already exists.'),
-            ]
+    _defaults = {
+        'active': True,
+        }
+
+    _sql_constraints = []
 
 
 class hr_workers_comp_history(osv.Model):
-    "workers comp  history"
+    "workers comp history"
     _name = 'hr.workers_comp.history'
     _desc = "workers comp claim history entry"
     _order = "evaluation_date"
@@ -325,14 +327,67 @@ class hr_workers_comp_history(osv.Model):
         'claim_id': fields.many2one('hr.workers_comp.claim', 'Claim #'),
         'create_date': fields.date('Date note entered', readonly=True),
         'write_uid': fields.many2one('res.users', 'Entered by'),
-        'evaluation_date': fields.date('Effective Date', help='date this change takes effect', oldname='effective_date'),
-        'note': fields.text('Note'),
-        'duty_id': fields.many2one('hr.workers_comp.duty_type', 'Duty Level'),
+        'evaluation_date': fields.date('Effective Date', help='date this entry takes effect', required=True, oldname='effective_date'),
+        'note': fields.text('Note', required=True),
+        'restriction': fields.selection((
+                ('full', 'No duties'),
+                ('light', 'Light Duties'),
+                ('none', 'Regular duties'),
+                ('est', 'Estimate'),
+                ('na', 'N/A'),
+            ),
+            'Restriction level',
+            required=True,
+            ),
+        'duty_id': fields.many2one('hr.workers_comp.duty_type', 'Duty Level', required=True),
         }
 
-    _defaults = {
-        'evaluation_date': fields.date.context_today,
-        }
+    def onchange_restriction(self, cr, uid, ids, restriction, duty_id, context=None):
+        """
+        if restriction is full, set duty_id to #restriction_full;
+        otherwise, exclued #restriction_full from the allowed choices.
+        """
+        ir_model_data = self.pool.get('ir.model.data')
+        records = ir_model_data.read(cr, uid, [('model','=','hr.workers_comp.duty_type')], fields=['name', 'res_id'], context=context)
+        active = [r['res_id'] for r in records]
+        full_restriction = [r['res_id'] for r in records if r['name'] == 'full_restriction']
+        light_restriction = [r['res_id'] for r in records if r['name'].startswith('restriction')]
+        no_restriction = [r['res_id'] for r in records if r['name'] not in ('incomplete', 'full_restriction')]
+        estimate = [r['res_id'] for r in records if r['name'] == 'employee_cleared_to_work']
+        #
+        res = {}
+        domain = res['domain'] = {}
+        value = res['value'] = {}
+        #
+        if restriction == 'full':
+            domain['duty_id'] = [('id','in',full_restriction)]
+            value['duty_id'] = duty_id = full_restriction[0]
+            restriction_ids = full_restriction
+        elif restriction == 'light':
+            domain['duty_id'] = [('id','in',light_restriction)]
+            restriction_ids = light_restriction
+        elif restriction == 'none':
+            domain['duty_id'] = [('id','in',no_restriction)]
+            restriction_ids = no_restriction
+        elif restriction == 'est':
+            domain['duty_id'] = [('id','in',estimate)]
+            value['duty_id'] = duty_id = estimate[0]
+            restriction_ids = estimate
+        elif restriction == 'na':
+            domain['duty_id'] = [('id','=',0)]
+            value['duty_id'] = duty_id = False
+            restriction_ids = []
+        else: # restriction is False
+            if duty_id and duty_id not in active:
+                # but duty is not, and is an old code
+                restriction_ids = [duty_id]
+            else:
+                value['duty_id'] = duty_id = False
+                restriction_ids = []
+        #
+        if duty_id not in restriction_ids:
+            value['duty_id'] = False
+        return res
 
 
 class workers_comp_hr(osv.Model):
@@ -458,3 +513,9 @@ class DayCounter(object):
 
     def __repr__(self):
         return 'DayCounter(%r)' % self.value
+
+
+class Restriction(NamedTuple):
+    id = 0
+    name = 1
+    restriction = 2
